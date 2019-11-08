4393ba9df15547d4a6d43c96156ba5ef30053259
Sean <sye40@gatech.edu>
2019-11-08 16:07:03 -0500

    pulled out background image to different function

--- color_threshold_attempt_wrapped.py
+++ color_threshold_attempt_wrapped.py
@@ -68,9 +68,70 @@ class ColorThresholdAttempt:
             im = self.resize_image(im, 0.25)
             self.process_image(im)
 
+    def crop_image_to_box_region(self, im):
+        ''' input raw image, return region of image that has the box inside '''
+        #step 1 gaussian blur
+        im = cv2.GaussianBlur(im,(7,7), 7)
+        
+        #step 2 green threshold for box
+        background_mask = self.threshold_color(im, self.green_threshold_values)
+        # self.preview(background_mask)
+
+        #find contours and select the contour with the greatest area
+        im2, contours, hierarchy = cv2.findContours(background_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        area = []
+        for i in range(len(contours)):
+            area.append(cv2.contourArea(contours[i]))
+            
+        cont = sorted(contours, key = cv2.contourArea, reverse = True)
+        cont_pic = im.copy()
+        cv2.drawContours(cont_pic, cont[0], -1, (0,255,0), 3)
+        # self.preview(cont_pic)
+        
+        bucket_cont = np.asarray(cont[0])
+
+        #grab the max & min X-Y values of the largest green contour
+        x_vals = []
+        y_vals = []
+        for i in range(len(bucket_cont)):
+            x_vals.append(bucket_cont[i][0][0])
+            y_vals.append(bucket_cont[i][0][1])            
+        
+
+        # self.RANSAC_slope_detection(cont_pic, x_vals, y_vals)
+
+        # x_1 = max(x_vals)
+        # x_max_ind = x_vals.index(max(x_vals))
+        # y_1 = y_vals[x_max_ind]
+        # cv2.circle(cont_pic,(y_1,x_1), 20, (0,0,255), -1)
+        
+        #crop additional space off of the max values
+        #figure out how to transform image/homography to overhead view
+        cut_pad = 40    
+        x_max = max(x_vals) - cut_pad
+        x_min = min(x_vals) + cut_pad
+        y_max = max(y_vals) - cut_pad
+        y_min = min(y_vals) + cut_pad
+        
+        #crop to the sized of the max values on that contour
+        ROI = im[y_min:y_max,x_min:x_max]
+        # self.preview(ROI)
+
+        return ROI
+
+
     def process_image(self, im):
         """ given opencv/numpy image, run the processing method """
+        # self.preview(im)
+        im = self.crop_image_to_box_region(im)
         self.preview(im)
+
+
+        # return return_image
+
+
+
 
     def RANSAC_slope_detection(self, cont_pic, x_vals, y_vals):
         """ compute slopes of boundary detection given image and contour points """
@@ -166,20 +227,20 @@ class ColorThresholdAttempt:
             ROI = im[y_min:y_max,x_min:x_max]
             self.preview(ROI)
             
-            green_mask = np.zeros(ROI.shape)
-            green_mask[:,:,0] = 32
-            green_mask[:,:,1] = 100
-            green_mask[:,:,2] = 1
-            green = np.asarray(green_mask, dtype = 'uint8')
-            white = self.threshold_color(ROI, self.white_threshold_values)
-            result = cv2.bitwise_and(green,ROI, mask= white)
-            for i in range(result.shape[0]):
-                for j in range(result.shape[1]):
-                    if sum(result[i,j,:]) == 0:
-                        result[i,j,:] = [255,255,255]
-            result2 = cv2.bitwise_and(result,ROI)
-            #blue_thresh = threshold_blue(ROI)
-            #preview(blue_thresh) 
+            # green_mask = np.zeros(ROI.shape)
+            # green_mask[:,:,0] = 32
+            # green_mask[:,:,1] = 100
+            # green_mask[:,:,2] = 1
+            # green = np.asarray(green_mask, dtype = 'uint8')
+            # white = self.threshold_color(ROI, self.white_threshold_values)
+            # result = cv2.bitwise_and(green,ROI, mask= white)
+            # for i in range(result.shape[0]):
+            #     for j in range(result.shape[1]):
+            #         if sum(result[i,j,:]) == 0:
+            #             result[i,j,:] = [255,255,255]
+            # result2 = cv2.bitwise_and(result,ROI)
+            # #blue_thresh = threshold_blue(ROI)
+            # #preview(blue_thresh) 
             
             green_thresh = self.threshold_color(ROI, self.green_threshold_values)
             #preview(green_thresh)
@@ -202,6 +263,9 @@ class ColorThresholdAttempt:
 
 if __name__ == '__main__':
     attempt = ColorThresholdAttempt()
-    attempt.run()
+    # attempt.run()
     path = 'bin_images-jpg'
     # attempt.process_images_from_folder(path)
+    im = cv2.imread(path + '/IMG_5040.jpg')
+    im = attempt.resize_image(im, 0.25)
+    attempt.process_image(im)
